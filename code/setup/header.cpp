eb92d4ac82ac378c137acf85e41b53b14c89716e
Jelmer Firet <spaanse.aak@gmail.com>
2020-10-25 13:16:33 +0100

    Add C++ header

---
+++ code/setup/header.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define EPS 1e-9
+#define INF (1<<29)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef int64_t ll;
+int main() {
+	iostream::sync_with_stdio(false);
+	cin.tie(0);
+//92b
+	return 0;
+}
