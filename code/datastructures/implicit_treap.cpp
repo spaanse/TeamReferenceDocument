48c20d02082c9e06119d4233fa4689cfb5d42d55
Jelmer Firet <spaanse.aak@gmail.com>
2021-01-11 09:38:45 +0100

    Remove wrapper around implicit treap to make more applicable

--- code/datastructures/implicit_treap.cpp
+++ code/datastructures/implicit_treap.cpp
@@ -13,23 +13,22 @@ typedef pair<int,int> ii;
 typedef vector<ii> vii;
 typedef int64_t ll;
 //727
-template<typename T>struct impl_treap{
-struct nd{T v;int h,s=1;bool f=0;
-	nd*l=NULL,*r=NULL;
-	nd(T _v){v=_v;h=rand();}}*s;
+struct nd{
+int v;int h,s=1,f=0;nd*l,*r=l=NULL;
+nd(int _v){v=_v;h=rand();}};
 int sz(nd*n){return n?n->s:0;}
 void upd(nd*n){
 	if(n)n->s=1+sz(n->l)+sz(n->r);}
-nd*cpy(nd*t){
+nd *cpy(nd*t){
 	if(!t) {return NULL;} nd*c=new nd(*t);
 	c->l=cpy(t->l);c->r=cpy(t->r);return c;}
 void del(nd*&t){if(!t)return;
-	del(t->l);del(t->r);t=NULL;}
+	del(t->l);del(t->r);delete t;t=NULL;}
 void ps(nd*t){if(!t||!t->f)return;
 	swap(t->l,t->r);t->f=0;
 	if(t->l){t->l->f^=1;} if(t->r)t->r->f^=1;}
-void spt(nd*t,nd*&l,nd*&r,
-	 int k,int d=0){if(!t) {l=r=NULL; return;}
+void spt(nd*t,nd*&l,nd*&r,int k,int d=0){
+	if(!t) {l=r=NULL; return;}
 	ps(t);int ck=d+sz(t->l);
 	if(ck<k) {spt(t->r,t->r,r,k,ck+1);l=t;}
 	else{spt(t->l,l,t->l,k,d);r=t;}upd(t);}
@@ -43,54 +42,45 @@ void ins(nd*&t,nd*n,int k,int d=0){
 	if(n->h>t->h)spt(t,n->l,n->r,k,d),t=n;
 	else if(k<=ck)ins(t->l,n,k,d);
 	else{ins(t->r,n,k,ck+1);}upd(t);}
-void rem(nd*&t, nd*&r,
-	int k,int d=0){if(!t)return;
-	ps(t);int ck=d+sz(t->l);
+void rem(nd*&t, nd*&r,int k,int d=0){
+	if(!t) {return;} ps(t);int ck=d+sz(t->l);
 	if(ck==k) {r=t;cc(t,t->l,t->r);}
 	if(ck>k) rem(t->l,r,k,d);
 	if(ck<k) {rem(t->r,r,k,ck+1);} upd(t);}
-nd*find(nd*t,int k,int d=0){
+nd *at(nd*t,int k,int d=0){
 	ps(t);int ck=d+sz(t->l);
 	if(!t||k==ck)return t;
-	if(k<ck)return find(t->l,k,d);
-	else    return find(t->r,k,ck+1);}
-impl_treap()= default;~impl_treap(){del(s);}
-impl_treap(impl_treap<T>&o){s=cpy(o.s);} 
-void insert(T v,int i){ins(s,new nd(v),i);}
-void remove(int i){nd*del=NULL;
-	rem(s,del,i);delete del;}
-void reverse(int l,int r){
-	nd*lt,*m,*rt=m=lt=NULL;
-	spt(s,lt,rt,r);spt(lt,lt,m,l);
-	if(m) {m->f^=1;} cc(lt,lt,m);cc(s,lt,rt);}
-size_t size(){return sz(s);}
-T&operator[](size_t i){
-	assert(0<=i&&i<size());
-	return find(s,i)->v;}};
+	if(k<ck)return at(t->l,k,d);
+	else    return at(t->r,k,ck+1);}
+void rev(nd*t,int l,int r){ nd*lt,*m,*rt;
+	spt(t,lt,rt,r);spt(lt,lt,m,l);
+	if(m) {m->f^=1;} cc(lt,lt,m);cc(t,lt,rt);}
 
 int main(){
-	impl_treap<int>arr;
+	nd *arr = 0;
 	for(int i=0;i<20;i++){
 		int newVal=rand()%100;
-		arr.insert(newVal,i);
+		ins(arr,new nd(newVal),i);
 		if (i) cout << " ";
 		cout << newVal;
 	} cout << endl;
-	arr.remove(3);arr.remove(5);arr.remove(5);
+	nd *tmp = 0;
+	rem(arr,tmp,3); delete tmp;
+	rem(arr,tmp,5); delete tmp;
+	rem(arr,tmp,5); delete tmp;
 	for (int i = 0; i < 17; i++) {
 		if (i) cout << " ";
-		cout << arr[i];
+		cout << at(arr,i)->v;
 	} cout << endl;
-	arr.reverse(5,10);
+	rev(arr,5,10);
 	for (int i = 0; i < 17; i++) {
 		if (i) cout << " ";
-		cout << arr[i];
+		cout << at(arr,i)->v;
 	} cout << endl;
-	impl_treap<int> arr2 = arr;
+	nd* arr2 = cpy(arr);
 	for (int i = 0; i < 17; i++) {
 		if (i) cout << " ";
-		cout << arr2[i];
+		cout << at(arr2,i)->v;
 	} cout << endl;
-
 	return 0;
 }
