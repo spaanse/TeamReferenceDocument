637b15fae54db7ca7d7196c837acd2a926a943c7
Jelmer Firet <spaanse.aak@gmail.com>
2021-03-25 21:27:07 +0100

    Improve min-queue

--- code/datastructures/min_queue.cpp
+++ code/datastructures/min_queue.cpp
@@ -13,26 +13,22 @@ typedef vector<ii> vii;
 typedef int64_t ll;
 //5ih
 // stack, but stores value of min element
-template<typename T> struct monStack{
-	stack<T> s, m;
-	bool size() {return s.size();}
-	T top(){return s.top();}
-	T min(){return m.top();}
-	void push(T v){ s.push(v);
-		m.push(m.size() ? min(m.top(),v) : 0);}
-	void pop() {s.pop(); m.pop();}};
+struct ms{stack<int> s,m;
+	int sz(){return s.sz();}
+	int top(){return s.sz()?s.top():2e9;}
+	int mn(){return s.sz()?m.top():2e9;}
+	void push(int v){ s.push(v);
+		m.push(min(m.top(),v));}
+	void pop(){s.pop();m.pop();}};
 // queue, but stores value of min element
-template<typename T> struct monQueue{
-	monStack<T> i,o;
-	bool size(){return i.size() + o.size();}
-	void fix(){if(!o.size())while(i.size()){
+struct mq{ms i,o;
+	int sz(){return i.sz()+o.sz();}
+	void fix(){if(!o.sz())while(i.sz()){
 			o.push(i.top()); i.pop();}}
-	T front() {fix(); return o.top();}
-	T min() { if (!i.size()) return o.min();
-		        if (!o.size()) return i.min();
-		return min(i.min(), o.min());}
-	void push(T v) {i.push(v);}
-	void pop() {fix(); o.pop();}};
+	int front() {fix(); return o.top();}
+	int mn(){return min(i.mn(), o.mn());}
+	void push(int v){i.push(v);}
+	void pop(){fix(); o.pop();}};
 
 int main() {
 	
