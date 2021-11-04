64d7494bcff3d4fc740f8fdfffaa6986a9256e8f
Jelmer Firet <spaanse.aak@gmail.com>
2021-11-04 18:13:00 +0100

    Shorten push-relabel

--- code/graphs/pushrelabel.cpp
+++ code/graphs/pushrelabel.cpp
@@ -9,21 +9,17 @@ typedef int64_t ll;
 typedef queue<int> qi;
 
 //faL
-struct flow{int s,t;vi l,x,i;vvi A,C,F;qi q;
-flow(int n):l(n),x(n),i(n),A(n),C(n,l),F(C){
-	s=0;t=n-1;l[s]=n;x[s]=2e9;}
-void add(int u,int v,int c){C[u][v]+=c;
-	A[u].push_back(v);A[v].push_back(u);}
+struct flow{int n,s,t;vi l,x,i;vvi C,F;qi q;
+flow(int N):l(N),x(N),i(N),C(N,l),F(C){
+	n=N;s=0;t=N-1;l[s]=N;x[s]=2e9;}
 void ps(int u,int v){if(l[v]>=l[u]) return;
 	int f=min(x[u],C[u][v]-F[u][v]);
 	if(!x[v]&&v!=s&&v!=t&&0<f)q.push(v);
 	F[u][v]+=f;F[v][u]-=f;x[v]+=f;x[u]-=f;}
-void fix(int u){while(x[u]){
-	if(i[u]==(int)A[u].size()){l[u]++;i[u]=0;}
-	else ps(u,A[u][i[u]++]);} i[u]--;}
-int calc(){for(int v:A[s]) ps(s,v);
-	for(;q.size();q.pop())fix(q.front());
-	return x[t];}};
+void fix(int u){while(x[u]){if(i[u]<n)
+	ps(u,i[u]++);else{l[u]++;i[u]=0;}}i[u]--;}
+void calc(){for(int v=0;v<n;v++) ps(s,v);
+	for(;q.size();q.pop())fix(q.front());}};
 
 int main(){
 	int numNode, numEdge;
@@ -32,12 +28,13 @@ int main(){
 	for (int i = 0; i < numEdge; i++) {
 		int fr,to,cap;
 		cin >> fr >> to >> cap;
-		fg.add(fr,to,cap);
+		fg.C[fr][to] += cap;
 	}
-	cout << fg.calc() << endl;
+	fg.calc();
+	cout << fg.x[numNode-1] << endl;
 
 	for(int fr=0;fr<numNode;fr++) {
-		for (int to : fg.A[fr]) {
+		for (int to=0; to < numNode; to++) {
 			assert(fg.C[fr][to]>=fg.F[fr][to]);
 		}
 		if (fr != 0 && fr != numNode-1) {
@@ -53,7 +50,7 @@ int main(){
 	while (!q.empty()) {
 		int fr = q.front(); q.pop();
 		assert(fr != numNode-1);
-		for (int to : fg.A[fr]) {
+		for (int to; to < numNode; to++) {
 			if (fg.C[fr][to]>fg.F[fr][to]
 				  &&!vis[to]) {
 				q.push(to); vis[to] = true;
